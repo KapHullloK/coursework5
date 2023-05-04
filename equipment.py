fb88435749885d63927ea2128e423dc6d91f7dfb
afro_0 <kirillov2136@gmail.com>
2023-05-04 15:24:41 +0300

    1 start

---
+++ equipment.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+from typing import List
+from random import uniform
+import marshmallow_dataclass
+import marshmallow
+import json
+
+
+@dataclass
+class Weapon:
+    id: int
+    name: str
+    min_damage: float
+    max_damage: float
+    stamina_per_hit: float
+
+    @property
+    def damage(self):
+        return uniform(self.min_damage, self.max_damage)
+
+
+@dataclass
+class Armor:
+    id: int
+    name: str
+    defence: float
+    stamina_per_turn: float
+
+
+@dataclass
+class EquipmentData:
+    weapons: List[Weapon]
+    armors: List[Armor]
+
+
+class Equipment:
+
+    def __init__(self):
+        self.equipment = self._get_equipment_data()
+
+    @staticmethod
+    def _get_equipment_data() -> EquipmentData:
+        # TODO этот метод загружает json в переменную EquipmentData
+        equipment_file = open("./data/equipment.json")
+        data = json.load(equipment_file)
+        equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
+        try:
+            return equipment_schema().load(data)
+        except marshmallow.exceptions.ValidationError:
+            raise ValueError
+
+    def get_weapon(self, weapon_name) -> Weapon:
+        # TODO возвращает объект оружия по имени
+        for weapon in self.equipment.weapons:
+            if weapon.name == weapon_name:
+                return weapon
+
+    def get_armor(self, armor_name) -> Armor:
+        for armor in self.equipment.armors:
+            if armor.name == armor_name:
+                return armor
+
+    def get_weapons_names(self) -> list:
+        # TODO возвращаем список с оружием
+        weapon_names = []
+        for weapon in self.equipment.weapons:
+            weapon_names.append(weapon.name)
+        return weapon_names
+
+    def get_armors_names(self) -> list:
+        return [armor.name for armor in self.equipment.armors]
